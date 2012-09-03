89cf879c25f8b2622e6222e746bb8bc706327d20
mockee <mockee@douban.com>
2012-09-03 13:49:34 +0800

    Remove static url prefix

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,27 @@
-var STATIC_URL = 'http://static.mockee.com';
-
 Do.add('sh-css', {
-  path: STATIC_URL + '/css/sh_kwrite.min.css',
+  path: '/css/sh_kwrite.min.css',
   type: 'css'
 });
 
 Do.add('sh', {
-  path: STATIC_URL + '/js/sh_main.min.js',
+  path: '/js/sh_main.min.js',
   requires: ['sh-css'],
   type: 'js'
 });
 
 Do.add('upload', {
-  path: STATIC_URL + '/js/upload.min.js',
+  path: '/js/upload.min.js',
   type: 'js'
 });
 
 Do.add('editor', {
-  path: STATIC_URL + '/js/editor.js',
+  path: '/js/editor.js',
   requires: ['upload'],
   type: 'js'
 });
 
 Do.add('comment', {
-  path: STATIC_URL + '/js/comment.js',
+  path: '/js/comment.js',
   type: 'js'
 });
 
