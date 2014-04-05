7a25948d7309e21e27f451f772eb109fcb3b7076
mockee <mockee@gmail.com>
2014-04-06 01:33:16 +0800

    Migrating express from 3.x to 4.x

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var path = require('path')
   , express = require('express')
+  , connect = require('connect')
   , mongoose = require('mongoose')
-  , MongoStore = require('connect-mongo')(express)
+  , MongoStore = require('connect-mongo')(connect)
   , istatic = require('express-istatic')
   , stylus = require('stylus')
 
@@ -14,99 +15,84 @@ var path = require('path')
 
   , app = module.exports = express()
   , Note, Comment, TrackBack, User, LoginToken
-  , pathPublic = path.join(__dirname, 'public');
+  , pathPublic = path.join(__dirname, 'public')
+  , env = process.env.NODE_ENV || 'development'
 
-app.configure(function() {
-  function compile(str, path) {
-    return stylus(str)
-      .set('filename', path)
-      // Data URI image inlining
-      .define('url', stylus.url({
-        paths: [pathPublic + '/img'] }))
-      .set('compress', true);
-  }
-  app.set('port', process.env.PORT || 8126);
-  app.set('view engine', 'jade');
-  app.use(stylus.middleware({
-    src: pathPublic,
-    dest: pathPublic,
-    compile: compile
-  }));
+  , bodyParser = require('body-parser')
+  , cookieParser = require('cookie-parser')
 
-  app.locals({
-    istatic: istatic.serve()
-  });
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    // Data URI image inlining
+    .define('url', stylus.url({
+      paths: [pathPublic + '/img'] }))
+    .set('compress', true)
+}
 
-  // set mongodb connection
-  app.set('connection', config.db.conn);
+app.set('port', process.env.PORT || 8126)
+app.set('view engine', 'jade')
+app.use(stylus.middleware({
+  src: pathPublic
+, dest: pathPublic
+, compile: compile
+}))
 
-  // Middleware
-  app.use(express.logger(
-    //:remote-addr:status:referrer
-    '\x1b[0m:date \x1b[32m:method\x1b[0m \x1b[33m:url\x1b[31m :response-time ms \x1b[36m:user-agent'));
+app.locals.istatic = istatic.serve()
 
-  app.use(express.bodyParser({
-    keepExtensions: true,
-    uploadDir: utils.getUploadPath()
-  }));
+// set mongodb connection
+app.set('connection', config.db.conn)
 
-  app.use(express.methodOverride());
+// Middleware
+app.use(connect.logger(
+  //:remote-addr:status:referrer
+  '\x1b[0m:date \x1b[32m:method\x1b[0m \x1b[33m:url\x1b[31m :response-time ms \x1b[36m:user-agent'))
 
-  app.use(express.cookieParser());
-  app.use(express.session({
-    cookie: { maxAge: 43200000 },
-    store: new MongoStore({ url: app.set('connection') }),
-    secret: 'nodecat'
-  }));
+app.use(bodyParser({
+  keepExtensions: true
+, uploadDir: utils.getUploadPath()
+}))
 
-  app.use(express.static(pathPublic));
-  app.set('views', __dirname + '/views');
+app.use(connect.methodOverride())
 
-  app.use(function(err, req, res, next) {
-    if (err instanceof utils.NotFound) {
-      res.render('error/404.jade', {
-        error: err,
-        title: err.name
-      });
-    } else if (err instanceof utils.InternalServerError) {
-      res.render('error/500.jade', {
-        error: err,
-        title: err.name
-      });
-    } else {
-      console.error(err);
-    }
-  });
-});
+app.use(cookieParser())
+app.use(connect.session({
+  cookie: { maxAge: 43200000 }
+, store: new MongoStore({ url: app.set('connection') })
+, secret: 'nodecat'
+}))
+
+app.use(express.static(pathPublic))
+app.set('views', __dirname + '/views')
 
 // $ NODE_ENV=development node app.js
-app.configure('development', function() {
-  app.set('port', 7901);
-  app.use(express.errorHandler({
-    dumpExceptions: true,
-    showStack: true
-  }));
-});
+if ('development' === env) {
+  app.set('port', 7901)
+  app.use(require('errorhandler')({
+    dumpExceptions: true
+  , showStack: true
+  }))
+}
 
 // Models
 noteModels.define(mongoose, function() {
-  Note = mongoose.model('Note');
-  Comment = mongoose.model('Comment');
-  TrackBack = mongoose.model('TrackBack');
-});
+  Note = mongoose.model('Note')
+  Comment = mongoose.model('Comment')
+  TrackBack = mongoose.model('TrackBack')
+})
 
 userModels.define(mongoose, function() {
-  User = mongoose.model('User');
-  LoginToken = mongoose.model('LoginToken');
-});
+  User = mongoose.model('User')
+  LoginToken = mongoose.model('LoginToken')
+})
 
 // Connection
-mongoose.connect(app.set('connection'));
+mongoose.connect(app.set('connection'))
 
 // Routes
-require('./router')(app, Note, Comment, TrackBack, User, LoginToken, config);
+require('./router')(app, Note, Comment, TrackBack, User, LoginToken, config)
 
 if (!module.parent) {
-  app.listen(app.set('port'));
-  console.info('Express server listening on port %d', app.get('port'));
+  app.listen(app.set('port'))
+  console.info('Express server listening on port %d', app.get('port'))
 }
